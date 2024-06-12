5d25d95fd30fbf0dc8957c5fe1ef1f55992d883c
Frankezecode <Franklynezeugonna@gmail.com>
2024-06-11 20:45:51 -0700

    Fixed array

---
+++ contracts/FixedArray.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.26;
+
+contract FixedSizeArray {
+    // Declare a fixed-size array of integers with 3 elements
+    uint[3] public numbers;
+    uint[3] public oddNumbers =[5,10,15];
+    uint[3] public evenNumbers;
+
+
+    
+
+    //Initialize the fixed array in the constructor
+    constructor() {
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        evenNumbers = [2, 4 ,6 ];
+    }
+
+    // Function to get an element at a specific index
+    function getElement(uint index1, uint index2, uint index3) public view returns (uint, uint, uint) {
+        return (numbers[index1], oddNumbers[index2] , evenNumbers[index3]);
+        
+    }
+}
