5d2a40e1d87da48a5ddf374790e9b21feee4265e
Frankezecode <Franklynezeugonna@gmail.com>
2024-06-11 21:06:14 -0700

    dynamic array

---
+++ contracts/DynamicArray.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity ^0.8.0;
+
+contract DynamicArray {
+    // Declare a dynamic array of integers
+    uint[] public numbers;
+
+    // Function to add an element to the array
+    function addElement(uint num) public {
+        numbers.push(num);
+    }
+
+    // Function to get an element at a specific index
+    function getElement(uint index) public view returns (uint) {
+        return numbers[index];
+    }
+
+    // Function to get the length of the array
+    function getLength() public view returns (uint) {
+        return numbers.length;
+    }
+}
