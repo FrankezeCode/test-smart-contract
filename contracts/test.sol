8256cf87863397d7f3292c09a8fb3dc55a256ed2
Frankezecode <Franklynezeugonna@gmail.com>
2024-06-11 19:08:04 -0700

    Mapping

--- contracts/test.sol
+++ contracts/test.sol
@@ -2,9 +2,20 @@
 pragma solidity ^0.8.26;
 
 
-contract Hello {
+contract Mapping {
 
-    string public  welcome = 'Hello World';
-    uint public num = 8;
-    string best = 'crpto';
+    
+    // Define a mapping from address to uint
+    mapping(address => uint) public   balances;
+
+    // Function to set the balance for a specific address
+    function setBalance(address _address, uint _balance) public {
+        balances[_address] = _balance;
+    }
+
+    // Function to get the balance of a specific address
+    function getBalance(address _address) public view returns (uint) {
+        return balances[_address];
+    }
+
 }
